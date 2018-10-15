1b2def9bddd5f353e6dc69d17d97d30ea05e129c
Soyoung Lee <soyoungl@andrew.cmu.edu>
2018-10-14 20:15:35 -0400

    Soyoung's Reflection

---
+++ Reflection.py
@@ -0,0 +1,13 @@
+# briefly describe your current understanding of Git
+
+print("""what are the murkiest points: sometime I feel I do things 
+without knowing what is actually happening. Just now I was trying 
+to submit the file and somehow it says the permission denied for
+ the directory that I made. Hopefully, I didnot make a directory, 
+ git-submission, in public
+
+- what do you feel confident in: what git can do and how to move things around using git-bash
+
+- what do you feel needs more work: how to undo/go back when I messed it up
+There is reset command but it is still unclear how much and what it is actually reseting""")
+
